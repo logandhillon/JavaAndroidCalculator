68d4e43e25d5a79cd9760f80bafc95dd258d20c7
Logan Dhillon <logandhi64@gmail.com>
2024-01-31 16:22:48 -0500

    Create main fragment Fragment imported from logandhillon/kotlin-android-calculator

---
+++ app/src/main/java/com/github/logandhillon/javaandroidcalculator/MainFragment.java
@@ -0,0 +1,54 @@
+/*
+ * JavaAndroidCalculator
+ * Copyright (C) 2024 Logan Dhillon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.github.logandhillon.javaandroidcalculator;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.github.logandhillon.javaandroidcalculator.databinding.FragmentMainBinding;
+
+public class MainFragment extends Fragment {
+    private FragmentMainBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.numpad1.setOnClickListener(v -> System.out.println("numpad1 pressed"));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
